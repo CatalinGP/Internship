d42b39808e2d6d9ae82297b3500a726faa5b5d9e
Tadeus23 <popescu351@gmail.com>
2023-12-21 09:03:00 +0200

    modified:   LinuxScripting/dbus.sh

--- LinuxScripting/dbus.sh
+++ LinuxScripting/dbus.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+echo "Monitoring power status changes..."
 
-BATTERY_PATH=$(upower -e | grep 'battery') 
+dbus-monitor --system "type='signal',interface='org.freedesktop.UPower',member='DeviceChanged'" |
+while read -r line; do
+    if [[ $line == *"power_supply_battery"* ]]; then
+        # Extract the status of the battery
+        STATUS=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep "state" | awk '{print $2}')
 
-while true; do
-    STATUS=$(upower -i "$BATTERY_PATH" | grep "state" | awk '{print $2}')
-
-    if [[ $STATUS == "discharging" ]]; then
-        echo "This machine is currently using the battery."
-    elif [[ $STATUS == "charging" ]] || [[ $STATUS == "fully-charged" ]]; then
-        echo "This machine is currently plugged in."
+        if [[ $STATUS == "discharging" ]]; then
+            echo "This machine is currently using the battery."
+        elif [[ $STATUS == "charging" ]] || [[ $STATUS == "fully-charged" ]]; then
+            echo "This machine is currently plugged in."
+        fi
     fi
-
-    sleep 5
 done
-
