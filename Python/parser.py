2741fdf10167d5602239e5de1621c22db0ea9c75
CatalinGP <catalin.popescu@randstaddigital.com>
2024-01-15 10:13:48 +0200

    modified:   Python/parser.py

--- Python/parser.py
+++ Python/parser.py
@@ -1,20 +1,14 @@
 def extract_last_words_in_timestamp_range(file_name, start_timestamp, end_timestamp):
-    last_words = []
-
     with open(file_name, 'r') as file:
-        for line in file:
-            parts = line.split()
-            if not parts:
-                continue
-
-            timestamp = parts[1]
-            if start_timestamp <= timestamp <= end_timestamp:
-                last_words.append(parts[-1])
-
-    return last_words
+        return [
+            parts[-1]
+            for line in file
+            if (parts := line.split()) and len(parts) > 1 and start_timestamp <= parts[1] <= end_timestamp
+        ]
 
 filename = "logcat_applications.txt"
 start = "17:56:07.996"
 end = "17:56:08.357"
 result = extract_last_words_in_timestamp_range(filename, start, end)
+
 print(*result, sep='\n')
