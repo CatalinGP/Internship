208fbb3efa8fbc328148dab2423901878e3137a8
CatalinGP <catalin.popescu@randstaddigital.com>
2024-01-24 10:19:03 +0200

    modified:   Python/ADAS_frame.py 	modified:   Python/IVI_frame.py

--- Python/ADAS_frame.py
+++ Python/ADAS_frame.py
@@ -8,81 +8,81 @@ class Signal:
 
 
 class Frame:
-    def __init__(self, frame_hex):
+    def __init__(self, frame_hex, lsb_first=True):
         self.frame_hex = frame_hex
-        self.frame_bin = self.hex_to_bin(frame_hex)
+        self.lsb_first = lsb_first
+        self.frame_bin = self.hex_to_bin(frame_hex, lsb_first)
 
-    @staticmethod
-    def hex_to_bin(hex_string):
+    def hex_to_bin(self, hex_string, lsb_first):
         binary_string = ""
         hex_bytes = hex_string.split()
         for hex_byte in hex_bytes:
-            binary_string += bin(int(hex_byte, 16))[2:].zfill(8)[::-1]
+            bits = bin(int(hex_byte, 16))[2:].zfill(8)
+            if not lsb_first:
+                bits = bits[::-1]
+            binary_string += bits
         return binary_string
 
-    @staticmethod
-    def bin_to_hex(bin_string):
-        return ' '.join(f"{int(bin_string[i:i +8][::-1], 2):02X}" for i in range(0, len(bin_string), 8))
+    def bin_to_hex(self):
+        hex_payload = ""
+        for i in range(0, len(self.frame_bin), 8):
+            byte = self.frame_bin[i:i+8]
+            if not self.lsb_first:
+                byte = byte[::-1]
+            hex_payload += f"{int(byte, 2):02X} "
+        return hex_payload.strip()
 
     def extract_signal_value(self, signal):
-        start_bit = (signal.byte * 8) + signal.bit
+        start_bit = signal.byte * 8 + signal.bit
         signal_bits = self.frame_bin[start_bit:start_bit + signal.size]
-        return int(signal_bits, 2)
+        return int(signal_bits[::-1] if self.lsb_first else signal_bits, 2)
 
-    def modify_signal_value(self, signal, new_value):
-        start_bit = (signal.byte * 8) + signal.bit
+    def modify_signal_value(self, signal):
+        start_bit = signal.byte * 8 + signal.bit
         end_bit = start_bit + signal.size
-        new_value_bin = f"{new_value:0{signal.size}b}"
+        new_value_bin = bin(signal.new_value)[2:].zfill(signal.size)
+        new_value_bin = new_value_bin[::-1] if self.lsb_first else new_value_bin
         self.frame_bin = self.frame_bin[:start_bit] + new_value_bin + self.frame_bin[end_bit:]
 
 
 class Decoder:
-    def __init__(self, signals):
+    def __init__(self, signals, lsb_first=True):
         self.signals = {sig['name']: Signal(**sig) for sig in signals}
+        self.lsb_first = lsb_first
 
-    def decode_and_modify_frame(self, frame_hex, modifications):
-        frame = Frame(frame_hex)
-        original_values = {signal.name: frame.extract_signal_value(signal) for signal in self.signals.values() }
-        for mod in modifications:
-            signal = self.signals.get(mod['name'])
-            if signal:
-                frame.modify_signal_value(signal, mod['new_value'])
-        modified_frame_hex = frame.bin_to_hex(frame.frame_bin)
+    def decode_and_modify_frame(self, frame_hex):
+        frame = Frame(frame_hex, self.lsb_first)
+        original_values = {}
+        for name, signal in self.signals.items():
+            original_values[name] = frame.extract_signal_value(signal)
+            if signal.new_value is not None:
+                frame.modify_signal_value(signal)
+        modified_frame_hex = frame.bin_to_hex()
         return original_values, modified_frame_hex
 
 
-# LDW_AlertStatus
-hex_frame1 = "80 00 00 00 00 00 00 00"
-hex_frame4 = "40 00 00 10 00 00 00 00"
-
-# DW_FollowUpTimeDisplay
-hex_frame2 = "FF 60 00 00 02 00 00 00"
-hex_frame5 = "21 20 00 00 02 00 00 00"
-
-# LCA_OverrideDisplay
-hex_frame3 = "80 00 00 00 00 00 00 00"
-hex_frame6 = "80 00 00 00 00 00 00 00"
-
 signals_info = [
-    {'name': 'LDW_AlertStatus', 'byte': 2, 'bit': 5, 'size': 2},
-    {'name': 'LCA_OverrideDisplay', 'byte': 5, 'bit': 2, 'size': 1},
-    {'name': 'DW_FollowUpTimeDisplay', 'byte': 4, 'bit': 7, 'size': 6}
+    {'name': 'LDW_AlertStatus', 'byte': 2, 'bit': 5, 'size': 2, 'new_value': 2},
+    {'name': 'LCA_OverrideDisplay', 'byte': 5, 'bit': 2, 'size': 1, 'new_value': 1},
+    {'name': 'DW_FollowUpTimeDisplay', 'byte': 4, 'bit': 7, 'size': 6, 'new_value': 45}
 ]
 
-decoder = Decoder(signals_info)
+frames = [
+    "80 00 00 00 00 00 00 00",
+    "40 00 00 10 00 00 00 00",
+    "FF 60 00 00 02 00 00 00",
+    "21 20 00 00 02 00 00 00",
+    "80 00 00 00 00 00 00 00",
+    "80 00 00 00 00 00 00 00"
+]
 
-modifications = {
-    'LDW_AlertStatus': 2,
-    'LCA_OverrideDisplay': 1,
-    'DW_FollowUpTimeDisplay': 45
-}
+# Seteaza lsb_first False daca bit 7 este LSB
+# Diferenta intre Little/Big Endian system
 
-frame = [hex_frame1, hex_frame2, hex_frame3, hex_frame4, hex_frame5, hex_frame6]
+decoder = Decoder(signals_info, lsb_first=True)
 
-for frame in frame:
-    frame_modifications = [{'name': key, 'new_value': value} for key, value in modifications.items()]
-    original_value, modified_frame = decoder.decode_and_modify_frame(frame, frame_modifications)
+for frame_hex in frames:
+    original_value, modified_frame = decoder.decode_and_modify_frame(frame_hex)
 
-    print(f"Original Frame: {frame}")
     print(f"Original Values: {original_value}")
     print(f"Modified Frame: {modified_frame}\n")
--- Python/IVI_frame.py
+++ Python/IVI_frame.py
@@ -7,42 +7,44 @@ class Signal:
 
 
 class Frame:
-    def __init__(self, frame_hex):
-        self.frame_hex = frame_hex
-        self.frame_bin = self.hex_to_bin(frame_hex)
+    def __init__(self, frame_hex, lsb_first=True):
+        self.frame_bin = self.hex_to_bin(frame_hex, lsb_first)
 
     @staticmethod
-    def hex_to_bin(hex_string):
+    def hex_to_bin(hex_string, lsb_first):
         binary_string = ""
         hex_bytes = hex_string.split()
         for hex_byte in hex_bytes:
-            binary_string += bin(int(hex_byte, 16))[2:].zfill(8)[::-1]
+            bits = bin(int(hex_byte, 16))[2:].zfill(8)
+            if not lsb_first:
+                bits = bits[::-1]
+            binary_string += bits
         return binary_string
 
     def extract_signal_value(self, signal):
-        start_bit = (signal.byte * 8) + signal.bit
+        start_bit = (signal.byte * 8) + (7 - signal.bit if signal.bit is not None else 0)
         signal_bits = self.frame_bin[start_bit:start_bit + signal.size]
         return int(signal_bits, 2)
 
 
 class Decoder:
-    def __init__(self, signals):
+    def __init__(self, signals, lsb_first=True):
         self.signals = [Signal(**sig) for sig in signals]
+        self.lsb_first = lsb_first
 
     def decode_payloads(self, payloads):
         decoded_payloads = []
         for payload_hex in payloads:
-            frame = Frame(payload_hex)
+            frame = Frame(payload_hex, self.lsb_first)
             signal_values = {signal.name: frame.extract_signal_value(signal) for signal in self.signals}
             decoded_payloads.append(signal_values)
-            return decoded_payloads
+        return decoded_payloads
 
 
 signals_info = [
     {'name': 'PassengerSeatMemoRequest', 'byte': 0, 'bit': 7, 'size': 3},
     {'name': 'ClimFPrightBlowingRequest', 'byte': 5, 'bit': 7, 'size': 4},
     {'name': 'TimeFormatDisplay', 'byte': 5, 'bit': 3, 'size': 1},
-
 ]
 
 payloads = [
@@ -50,7 +52,10 @@ payloads = [
     "40 12 6C AF 05 78 4A 04"
 ]
 
-decoder = Decoder(signals_info)
+# Seteaza lsb_first False daca bit 7 este LSB
+# Diferenta intre Little/Big Endian system
+
+decoder = Decoder(signals_info, lsb_first=True)
 decoded_values = decoder.decode_payloads(payloads)
 for d in decoded_values:
     print(d)
